bb217f7a2929971a47dc15f9b94d5627fdb8f841
FathurSaha12 <95715048+FathurSaha12@users.noreply.github.com>
2021-12-20 14:55:31 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,25 +1,53 @@
+let regex = /https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com\/.*/i
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'Uhm..url nya mana?'
-m.reply(wait)
-let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.status) throw json
-let { description, author, statistic, link } = json.result
-await conn.sendFile(m.chat, link, 'tt.mp4', `
-▶ ${statistic.playCount} Views
-❤ ${statistic.diggCount} Likes
-🔁 ${statistic.shareCount} Shares
-💬 ${statistic.commentCount} Comments
-- *By:* ${author.nickname} (${author.username})
-- *Desc:*
-${description}
-`.trim(), m)
-}
 
-handler.help = ['tiktok <url>']
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `uhm.. urlnya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/ZGJBtcsDq/`
+    if (!regex.test(args[0])) throw 'url salah'
+
+    let json = await tiktok2(args[0].match(regex)[0].split(/\n| /i)[0]).then(async res => {
+        let data = JSON.stringify(res)
+        let json = JSON.parse(data)
+        return json
+    })
+    if (!json.status) throw global.eror
+    await m.reply(global.wait)
+    await conn.sendFile(m.chat, json.data.videoSD, 'tiktok.mp4', watermark, m, 0, {mimetype: 'video/gif'})
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^tiktok$/i
+handler.command = /^(tiktok)$/i
+
+handler.limit = 1
 
 module.exports = handler
+
+function numbFormat(int) {
+    return Number(parseInt(int)).toLocaleString().replace(',', '.')
+}
+
+function tiktok2(url) {
+    let config = {
+        headers: {
+            'Accept': '*',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    }
+    return new Promise(async (resolve, reject) => {
+        let form = new URLSearchParams()
+        form.append('url', url)
+        let json = await (await fetch('https://api.tikmate.app/api/lookup', { method: 'POST', body: form, ...config })).json()
+        if (!json.success) return resolve({ status: false })
+        let postInfo = {
+            author: json.author_name + ' (@' + json.author_id + ')',
+            publish: json.create_time,
+            likes: numbFormat(json.like_count),
+            comments: numbFormat(json.comment_count),
+            shares: numbFormat(json.share_count),
+        }
+        let urlList = {
+            videoSD: 'https://tikmate.app/download/' + json.token + '/' + json.id + '.mp4',
+            videoHD: 'https://tikmate.app/download/' + json.token + '/' + json.id + '.mp4?hd=1'
+        }; resolve({ status: true, ...postInfo, data: urlList })
+    })
+}
