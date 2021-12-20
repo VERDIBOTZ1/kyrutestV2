d2b9e6729049bb062968886df9fc236effcf0c20
FathurSaha12 <95715048+FathurSaha12@users.noreply.github.com>
2021-12-20 14:49:41 +0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -72,6 +72,6 @@ handler.tags = ['jadibot', 'premium']
 handler.command = /^jadibot$/i
 handler.premium = false
 handler.private = true
-handler.limit = 800
+handler.limit = 100
 
 module.exports = handler
